2e50045831d29dcaa51f3d98338afeb44d79e862
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-10 11:47:28 +0100

    changing to a better code

--- src/components/PhotosAppScriptClass1.jsx
+++ src/components/PhotosAppScriptClass1.jsx
@@ -43,66 +43,27 @@ import cloudcomp from './cardsPhotos/cloudcomp.jpg'
 import flydata from './cardsPhotos/flyresult.jpg'
 import concurcarna2024 from './cardsPhotos/concursocarna2024.jpg'
 import redsea from './cardsPhotos/redsea.jpg'
+import quianty from './cardsPhotos/quianty.jpg'
 
 
 const PhotosAppScriptClass1 = () => {
+
+  let arrayOfPhotos = [people,microscope,arte,deporte ,estudiantes,evento,entrevista, ai,ascensor,botella,
+    congreso,cubo,marlene, empren,game,itsa, karam,medalla,mejores,purdu,semilleros,territorio,letanias,induc,sedeFan,
+    carnaval,uninews,concejeria,korea2023,induc12023,guacher2024,city,dospuentes,etica20231,tech12024,peoplenews,robot,economysup,cyberseg,
+    cloudcomp,flydata,concurcarna2024,redsea,quianty]
+
+    console.log(arrayOfPhotos.length)
   return (
     <article className="main-photo1">
       <h2>Data base of cars photos</h2>
       <div className='main-photo1-div'>
-        <img src={people} alt="" />
-        <img src={microscope} alt="" />
-        <img src={arte} alt="" />
-        <img src={deporte} alt="" />
-        <img src={estudiantes} alt="" />
-        <img src={evento} alt="" />
-        <img src={entrevista} alt="" />
-        <img src={ai} alt="" />
-
-        <img src={ascensor} alt="" />
-        <img src={botella} alt="" />
-        <img src={congreso} alt="" />
-        <img src={cubo} alt="" />
-        <img src={marlene} alt="" />
-        <img src={empren} alt="" />
-        <img src={game} alt="" />
-        <img src={itsa} alt="" />
-        <img src={karam} alt="" />
-
-        
-        <img src={medalla} alt="" />
-        <img src={mejores} alt="" />
-        <img src={purdu} alt="" />
-        <img src={semilleros} alt="" />
-        <img src={territorio} alt="" />
-
-        <img src={letanias} alt="" />
-        <img src={induc} alt="" />
-        <img src={sedeFan} alt="" />
-
-        <img src={carnaval} alt="" />
-        <img src={uninews} alt="" />
-        <img src={concejeria} alt="" />
-
-        <img src={korea2023} alt="" />
-        <img src={induc12023} alt="" />
-        <img src={guacher2024} alt="" />
-
-        <img src={city} alt="" />
-        <img src={dospuentes} alt="" />
-
-        <img src={etica20231} alt="" />
-        <img src={tech12024} alt="" />
-        <img src={peoplenews} alt="" />
-
-        <img src={robot} alt="" />
-        <img src={economysup} alt="" />
-        <img src={cyberseg} alt="" />
-        <img src={cloudcomp} alt="" />
-
-        <img src={flydata} alt="" />
-        <img src={concurcarna2024} alt="" />
-        <img src={redsea} alt="" />
+        {
+          arrayOfPhotos.map((photo,i)=>(
+            <img key={i} src={photo} alt="" />
+          ))
+        }
+      
       </div>
     </article>
   )
